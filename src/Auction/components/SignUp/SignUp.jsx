d8483a0434a93c36dc2e9c18e7a93282d1960cf2
vjvjy <vijayaraajan@gmail.com>
2025-01-30 23:16:31 +0530

    build fail changes

--- src/Auction/components/SignUp/SignUp.jsx
+++ src/Auction/components/SignUp/SignUp.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useMatch, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp = () => {
