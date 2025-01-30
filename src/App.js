33fbb9e07ec5400eeef1e1240103459d39517bb8
vjvjy <vijayaraajan@gmail.com>
2025-01-30 22:48:59 +0530

    changing folder structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-import {store} from './../src/store'
-import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignUp from './Auction/components/SignUp';
-import Home from './Auction/components/Home';
+import SignUp from './Auction/components/SignUp/SignUp';
+import Home from './Auction/components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
+      {/* <Provider store={store}> */}
         <BrowserRouter>
         <Routes>
           <Route path="/" Component={SignUp} />
@@ -17,7 +15,7 @@ function App() {
         </Routes>
         </BrowserRouter>
       {/* <AuctionPage /> */}
-      </Provider>
+      {/* </Provider> */}
     </div>
   );
 }
---
+++ src/Auction/components/Home/Home.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+
+const Home = () => {
+    const [players, setPlayers] = useState([]);
+    const [header, setHeader] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/api/players")
+            .then((res) => res.json())
+            .then((data) => {
+                setPlayers(data);
+                setHeader(Object.keys(data[0]));
+            });
+    }, []);
+
+    return (
+        <div className="container">
+            <div className="table-container">
+                <p className="heading">You can view your player details here</p>
+                <table className="player-table">
+                    <thead>
+                        <tr>
+                            {header.map((item, index) => (
+                                <th key={index}>{item.toUpperCase()}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {players.map((item, index) => (
+                            <tr key={index}>
+                                {header.map((head, headIndex) => (
+                                    <td key={headIndex}>{item[head]}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <button className="auction-btn">Go To Auction</button>
+        </div>
+    );
+};
+
+export default Home;
