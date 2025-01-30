0f3925ae181205980b939d8fe4920ad36c5421a8
vjvjy <vijayaraajan@gmail.com>
2025-01-30 21:04:03 +0530

    build error from netlify - deleting unneccessary imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 // import logo from './logo.svg';
-import logo from './clipart1152511.png'
 import './App.css';
-import Parent from './Parent';
-import UserDetails from './UserDetails/UserDetails';
 import {store} from './../src/store'
 import { Provider } from 'react-redux'
-import NodeJs from './UserDetails/NodeJs';
-import AuctionPage from './Auction/components/AuctionPage';
-import UserTable from './UserTable';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUp from './Auction/components/SignUp';
 import Home from './Auction/components/Home';
