f9e84189b9e0fc51bdd6883d484a1b2a6574346c
vjvjy <vijayaraajan@gmail.com>
2025-01-30 21:45:17 +0530

    deleting unnecessary files

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { userDetailsReducer } from "./UserDetails/userDetailsSlice";
-
-export const store = configureStore({
-    reducer: {
-        user: userDetailsReducer,
-    },
-})
